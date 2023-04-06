48a668ce465142bff0326837cc76b4fc210bfd9d
Praveen Sake <sake@fb.com>
2023-04-06 13:05:54 -0400

    properties

--- src/com/perm/main/PermMain.java
+++ src/com/perm/main/PermMain.java
@@ -1,7 +1,36 @@
 package com.perm.main;
 
-public class Test {
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PermMain {
+    final static Logger logger = Logger.getLogger(PermMain.class);
+
     public static void main(String[] args) {
-        System.out.println("Perm Approved ");
+
+        try (InputStream input = PermMain.class.getClassLoader().getResourceAsStream("perm.properties")) {
+
+            Properties prop = new Properties();
+
+            if (input == null) {
+                System.out.println("Sorry, unable to find config.properties");
+                return;
+            }
+
+            //load a properties file from class path, inside static method
+            prop.load(input);
+
+            // Java 8 , print key and values
+            if(logger.isInfoEnabled()){
+                prop.forEach((key, value) -> logger.info("Key : " + key + ", Value : " + value));
+            }
+
+        } catch (Exception ex) {
+            logger.error("Unable to read properties file", ex);
+        }
+
     }
 }
